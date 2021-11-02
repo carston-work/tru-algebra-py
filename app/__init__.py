74199049957c6d0b066b3ac693b2689c2e94dea0
Carston Work <carston.work@gmail.com>
2021-11-02 09:23:46 -0600

    changes to get deployment working

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ def create_app():
     app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DEBUG'] = True
     
 
     app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ.get('RECAPTCHA_PUBLIC_KEY')
--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from random import randint
 main = Blueprint('main', __name__, template_folder="templates")
 
 
-@main.route('/', methods=['GET'])
+@main.route('/')
 def home():
     demo_id = randint(1, 10)
     return render_template('home.html', demo_id=demo_id)
